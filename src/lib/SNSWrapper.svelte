3c4d5e79d9db939791a733391197ae305320a2a5
Doug A <douganderson444@gmail.com>
2022-02-01 06:47:34 -0400

    mv inside mount

--- src/lib/SNSWrapper.svelte
+++ src/lib/SNSWrapper.svelte
@@ -7,40 +7,40 @@
 	let mounted;
 	let snsResolve;
 
-	const showhandleAccounts = async () => {
-		if (handle.length < 4) return;
-
-		if (handle.length < 44) {
-			// TODO: check naming service
-			if (!snsResolve) {
-				({ snsResolve } = await import('./sns-resolver')); // https://github.com/solana-labs/solana-program-library/blob/3e945798fc70e111b131622c1185385c222610fd/name-service/js/src/twitter.ts#L217
-			}
-
-			try {
-				const result = await snsResolve(handle);
-
-				if (!result) {
-					pubKey = ''; // clear pubkey results
-					return;
-				}
-				pubKey = result;
-				console.log(`Handle ${handle} points to `, pubKey.toBase58());
-			} catch (error) {
-				console.warn(`Handle ${handle} points to nothing.`);
-			}
-		}
-	};
+	let showhandleAccounts;
 
 	$: mounted && handle && showhandleAccounts();
 
 	onMount(async () => {
-		global = globalThis; // for solana web3 repo
-
 		const Buffer = await import('buffer'); // Solana Web3.js uses Buffers instead of UInt8Array =/
 		global.Buffer = Buffer.Buffer;
 
 		const process = await import('process');
 		global.process = process;
+
+		showhandleAccounts = async () => {
+			if (handle.length < 4) return;
+
+			if (handle.length < 44) {
+				// TODO: check naming service
+				if (!snsResolve) {
+					({ snsResolve } = await import('./sns-resolver')); // https://github.com/solana-labs/solana-program-library/blob/3e945798fc70e111b131622c1185385c222610fd/name-service/js/src/twitter.ts#L217
+				}
+
+				try {
+					const result = await snsResolve(handle);
+
+					if (!result) {
+						pubKey = ''; // clear pubkey results
+						return;
+					}
+					pubKey = result;
+					console.log(`Handle ${handle} points to `, pubKey.toBase58());
+				} catch (error) {
+					console.warn(`Handle ${handle} points to nothing.`);
+				}
+			}
+		};
 
 		mounted = true;
 	});
@@ -51,3 +51,5 @@
 		global = globalThis; // for solana web3 repo
 	</script>
 </svelte:head>
+
+<div />
